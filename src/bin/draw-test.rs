8c1b6671179a4393b97d14d1891fcb7c5d4c2429
Ben Kirwin <ben@kirw.in>
2021-07-10 16:05:52 -0400

    Clean up imports etc.

--- src/bin/draw-test.rs
+++ src/bin/draw-test.rs
@@ -1,19 +1,10 @@
 use std::fs;
-use std::sync::mpsc::channel;
 
-use libremarkable::cgmath::{EuclideanSpace, Point2};
-use libremarkable::framebuffer::{core, FramebufferDraw};
-use libremarkable::framebuffer::FramebufferBase;
-use libremarkable::input::{InputDevice, InputEvent};
-use libremarkable::input::ev::EvDevContext;
+use libremarkable::framebuffer::cgmath::Vector2;
 use rusttype::Font;
 
-use armrest::{gesture, ui};
-use armrest::geom::BoundingBox;
-use armrest::gesture::{Gesture, Tool};
-use armrest::ui::{Screen, Widget};
 use armrest::app;
-use libremarkable::framebuffer::cgmath::Vector2;
+use armrest::ui;
 
 fn main() {
     let font_bytes =
@@ -21,11 +12,11 @@ fn main() {
 
     let font: Font<'static> = Font::from_bytes(font_bytes).unwrap();
 
-    let mut lines = ui::Text::wrap(
+    let lines = ui::Text::wrap(
         &font,
         &"and but that blow would be the be-all and the end-all here, then here, upon this bank and shoal of time, we'd jump the life to come. ".repeat(10),
         1000,
-        44
+        44,
     );
 
     let mut stack = ui::Stack::new(Vector2::new(1000, 1800));
@@ -34,7 +25,7 @@ fn main() {
         stack.push(line.on_touch(Some(i)));
     }
 
-    app::run_widget(stack, |stack, action, message| {
+    app::run_widget(stack, |stack, _action, message| {
         let next = ui::Text::layout(&font, &format!("Touched line {:?}", message), 44);
         stack.push(next.on_touch(Some(message)));
         Ok(())
