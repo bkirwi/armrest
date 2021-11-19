eb4b3da604a4c71b9440e9ac0976b383eb32624f
Ben Kirwin <ben@kirw.in>
2021-11-19 10:48:05 -0500

    Change the helper script to reference the latest toolchain

--- build-rm.sh
+++ build-rm.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
+set -e
+
 if [ -z "${RM_TOOLCHAIN+x}" ]; then
   echo "Set the RM_TOOLCHAIN env var to the toolchain path!"
   exit 1
 fi
 
 # Set up env vars from the standard rm toolchain
-source $RM_TOOLCHAIN/environment-setup-cortexa9hf-neon-oe-linux-gnueabi
+source $RM_TOOLCHAIN/environment-setup-cortexa7hf-neon-remarkable-linux-gnueabi
 
 set -euo pipefail
 
 # This is needed to cross-build with bindgen
 export BINDGEN_EXTRA_CLANG_ARGS="\
-  -I$RM_TOOLCHAIN/sysroots/cortexa9hf-neon-oe-linux-gnueabi/usr/include/c++/7.3.0\
-  -I$RM_TOOLCHAIN/sysroots/cortexa9hf-neon-oe-linux-gnueabi/usr/include\
-  -I$RM_TOOLCHAIN/sysroots/cortexa9hf-neon-oe-linux-gnueabi/usr/include/c++/7.3.0/arm-oe-linux-gnueabi"
+  -I$RM_TOOLCHAIN/sysroots/cortexa7hf-neon-remarkable-linux-gnueabi/usr/include/c++/9.3.0\
+  -I$RM_TOOLCHAIN/sysroots/cortexa7hf-neon-remarkable-linux-gnueabi/usr/include\
+  -I$RM_TOOLCHAIN/sysroots/cortexa7hf-neon-remarkable-linux-gnueabi/usr/include/c++/9.3.0/arm-remarkable-linux-gnueabi"
 #  --sysroot=$RM_TOOLCHAIN/sysroots/cortexa9hf-neon-oe-linux-gnueabi"
 
 # Arguments for the tensorflow build in the build.rs file of tflite-rs.
